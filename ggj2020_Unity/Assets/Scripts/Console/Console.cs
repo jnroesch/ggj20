15b554f67d20e67ac41bfa3c442efa5623fc41e7
Ludwig Seibt <ludwigseibt@gmail.com>
2020-01-31 23:48:29 +0100

    console wip

---
+++ ggj2020_Unity/Assets/Scripts/Console/Console.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Console : MonoBehaviour
+{
+    public GameObject logEntryPrefab;
+    public Transform logEntryParent;
+    public InputField inputField;
+
+    public List<Text> logEntries = new List<Text>();
+
+    private void Awake()
+    {
+        inputField = GetComponent<InputField>();
+        inputField.text = ">";
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        inputField.ActivateInputField();
+    }
+
+    public void CheckForLineEnd(string currentInputText)
+    {
+        if (currentInputText.EndsWith("\n"))
+        {
+            CreateLogEntry(currentInputText);
+        }
+    }
+
+    public void CreateLogEntry(string logEntryText)
+    {
+        Text logEntry = Instantiate(logEntryPrefab, logEntryParent).GetComponent<Text>();
+        logEntry.text = logEntryText;
+
+        logEntries.Add(logEntry);
+
+        inputField.text = ">";
+        //inputField.ActivateInputField();
+    }
+}
