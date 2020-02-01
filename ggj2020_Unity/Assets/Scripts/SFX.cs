88efcde6ea402fc4dd7fbeb52830c8a12401823d
Ludwig Seibt <ludwigseibt@gmail.com>
2020-02-01 23:12:57 +0100

    sfx wip

---
+++ ggj2020_Unity/Assets/Scripts/SFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SFX : MonoBehaviour
+{
+    public AudioClip BackgroundMusic;
+
+    public AudioClip TimerBeep;
+
+    [SerializeField]
+    private AudioClip[] _randomBeeps;
+
+    public AudioClip GetRandomBeep()
+    {
+        System.Random rnd = new System.Random();
+        int rndInt = rnd.Next(_randomBeeps.Length);
+        return _randomBeeps[rndInt];
+    }
+}
